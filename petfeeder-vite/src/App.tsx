18fe584dc3537a5d7d806c76d05aba97e29b6d30
takojakos <dziki.sledz@op.pl>
2025-07-07 20:53:00 +0200

    Deleted the form element as it was useless and addad a site title

--- petfeeder-vite/src/App.tsx
+++ petfeeder-vite/src/App.tsx
@@ -9,19 +9,16 @@ export default function App() {
       <h1>Pet Feeder</h1>
       <p>Welcome to the Pet Feeder application!</p>
       <p>Please select how often would you like to feed your pet</p>
-      <form>
-        <select name="feedFrequency" id="feedFrequency" onChange={(e) => setFeedFrequency(e.target.value)} value={feedFrequency}>
-          <option value="1">Once a day</option>
-          <option value="2">Twice a day</option>
-          <option value="3">Every Other Day</option>
-          <option value="4">Every Three Days</option>
-          <option value="5">Once a week</option>
-          <option value="6">Twice a week</option>
-          <option value="7">Every Two Weeks</option>
-          <option value="8">Once a month</option>
-        </select>
-        <button type="submit">Submit</button>
-      </form>
+      <select name="feedFrequency" id="feedFrequency" onChange={(e) => setFeedFrequency(e.target.value)} value={feedFrequency}>
+        <option value="1">Once a day</option>
+        <option value="2">Twice a day</option>
+        <option value="3">Every Other Day</option>
+        <option value="4">Every Three Days</option>
+        <option value="5">Once a week</option>
+        <option value="6">Twice a week</option>
+        <option value="7">Every Two Weeks</option>
+        <option value="8">Once a month</option>
+      </select>
       {feedFrequency ?
         <>
           <h5>Great you selected {feedFrequency} as your option</h5>
