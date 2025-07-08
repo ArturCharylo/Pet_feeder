1f517170348dc1d36486eea9093dc1854c2b80f8
takojakos <dziki.sledz@op.pl>
2025-07-08 08:01:14 +0200

    Set up PWA

--- petfeeder-vite/vite.config.ts
+++ petfeeder-vite/vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pet Feeder',
+        short_name: 'Feeder',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0084ff',
+        icons: [
+          {
+            src: './Pet_feeder.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './Pet_feeder.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
