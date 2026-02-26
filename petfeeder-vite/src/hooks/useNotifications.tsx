4b1528a44e19273513e15b78f635c55c42716669
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-02-26 19:10:12 +0100

    patch: deleted console.logs from useNotifications hook

--- petfeeder-vite/src/hooks/useNotifications.tsx
+++ petfeeder-vite/src/hooks/useNotifications.tsx
@@ -2,7 +2,6 @@ export const requestNotificationPermission = async () => {
   if ('Notification' in window) {
     try {
       const permission = await Notification.requestPermission();
-      console.log('Notification permission:', permission);
       return permission;
     } catch (error) {
       // Handle potential errors in older browsers
@@ -14,12 +13,11 @@ export const requestNotificationPermission = async () => {
 };
 
 export const sendNotification = (title: string, options?: NotificationOptions) => {
-  console.log('Try send notification, permission:', Notification.permission);
   if ('Notification' in window && Notification.permission === 'granted') {
     // Return the instance to allow closing it later if needed
     return new Notification(title, options);
   } else {
-    console.log('Notification not sent, permission:', Notification.permission);
+    console.error('Notification not sent, permission:', Notification.permission);
     return null;
   }
 };
